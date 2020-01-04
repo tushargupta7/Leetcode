f9d43247b9a0cdebfc82698d2f1575637ca206cf
Tushar Gupta <tusharg@ridlr.in>
2020-01-04 10:08:12 +0530

    LongestCommonSubsequence code

---
+++ src/com/company/dynamic/LongestCommonSubsequence.java
@@ -0,0 +1,36 @@
+package com.company.dynamic;
+
+public class LongestCommonSubsequence {
+    public static void main(String[] args)
+    {
+        LongestCommonSubsequence lcs = new LongestCommonSubsequence();
+        String s1 = "AGGTAB";
+        String s2 = "GXTXAYB";
+
+        char[] X=s1.toCharArray();
+        char[] Y=s2.toCharArray();
+        int m = X.length;
+        int n = Y.length;
+
+        System.out.println("Length of LCS is" + " " +
+                lcs.lcs( X, Y, m, n ) );
+    }
+
+    private int lcs(char[] x, char[] y, int m, int n) {
+        int dp[][]=new int[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0||j==0){
+                    dp[i][j]=0;
+                }
+                else if(x[i-1]==y[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
